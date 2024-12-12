04ce7d5c1805a8940f5b1e028e0bba1625cfc028
Theunderdog <9027447@student.zadkine.nl>
2024-12-12 12:25:35 +0100

    Week 2 Cookie-melding en dark-mode toegevoegd

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 function Klik() {
-    var element = document.body;
-    element.classList.toggle("dark_mode");
+var element = document.body;
+element.classList.toggle("dark_mode");
 }
+
+function acceptCookies() {
+    console.log("Accept button clicked");
+    document.getElementById('cookie-banner').style.display = 'none';
+    console.log("Cookie banner hidden");
+
+    document.cookie = "cookieConsent=true; path=/; max-age=" + 60*60*24*30;
+    console.log("Cookie consent stored");
+}
+
+window.onload = function() {
+    console.log("Page loaded");
+    if (document.cookie.indexOf('cookieConsent=true') !== -1) {
+        console.log("Cookie consent already given");
+        document.getElementById('cookie-banner').style.display = 'none';
+    }
+};
+
